0f66e99cfa88193505788db77d019d3a1daf977e
Thalezv <thales---medeiros@hotmail.com>
2025-07-29 11:58:43 -0300

    atualização: envio de email php

--- envia.php
+++ envia.php
@@ -1,20 +1,21 @@
 <?php
-
-    $nome = addslashes($_POST['nome']);
-    $email = addslashes($_POST['email']);
-    $fone = addslashes($_POST['fone']);
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $nome = htmlspecialchars($_POST['nome'] ?? '');
+    $emailForm = filter_var($_POST['email'] ?? '', FILTER_SANITIZE_EMAIL);
+    $fone = htmlspecialchars($_POST['fone'] ?? '');
 
     $to = "thales---medeiros@hotmail.com";
-    $assunto = "leads";
+    $assunto = "Leads";
+    $mensagem = "Nome: $nome\nEmail: $emailForm\nTelefone: $fone";
 
-    $email = "Nome: " . $nome . "\n" . "Email: " . $email . "\n" . "Telefone: " . $fone;
+    $cabecalho = "From: teste@thales.com\r\n";
+    $cabecalho .= "Reply-To: $emailForm\r\n";
+    $cabecalho .= "X-Mailer: PHP/" . phpversion();
 
-    $cabeca = "From: teste@thales.com" . "\n" . "Reply-to: " . $email."\n" . "X=Mailer:PHP/" . phpversion();
-
-    if(mail($to, $assunto, $email, $cabeca)){
-        echo("Email enviado!");
-    }else{
-        echo("Houve erro!");
+    if (mail($to, $assunto, $mensagem, $cabecalho)) {
+        echo "Email enviado!";
+    } else {
+        echo "Houve erro!";
     }
-
+}
 ?>
