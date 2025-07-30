d5bab94a215b17afecd87acc73e3c34a00d7dc16
Thalezv <thales---medeiros@hotmail.com>
2025-07-30 11:15:48 -0300

    Update: metodo melhor de envio de email

--- envia.php
+++ envia.php
@@ -1,21 +1,41 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $nome = htmlspecialchars($_POST['nome'] ?? '');
     $emailForm = filter_var($_POST['email'] ?? '', FILTER_SANITIZE_EMAIL);
     $fone = htmlspecialchars($_POST['fone'] ?? '');
 
-    $to = "thales---medeiros@hotmail.com";
-    $assunto = "Leads";
-    $mensagem = "Nome: $nome\nEmail: $emailForm\nTelefone: $fone";
+    $mail = new PHPMailer(true);
 
-    $cabecalho = "From: teste@thales.com\r\n";
-    $cabecalho .= "Reply-To: $emailForm\r\n";
-    $cabecalho .= "X-Mailer: PHP/" . phpversion();
+    try {
+        // Configurações do servidor SMTP
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = '***';
+        $mail->Password = '***';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
 
-    if (mail($to, $assunto, $mensagem, $cabecalho)) {
-        echo "Email enviado!";
-    } else {
-        echo "Houve erro!";
+        // Remetente e destinatário
+        $mail->setFrom('***', 'Formulário do Site Teste');
+        $mail->addAddress($emailForm, $nome);
+
+        // Conteúdo do e-mail
+        $mail->isHTML(false);
+        $mail->Subject = 'Novo lead do site';
+        $mail->Body = "Nome: $nome\nEmail: $emailForm\nTelefone: $fone";
+
+        $mail->send();
+        echo "✅ E-mail enviado com sucesso!";
+    } catch (Exception $e) {
+        echo "❌ Erro ao enviar: {$mail->ErrorInfo}";
     }
 }
 ?>
